a5e28b52172392ffc0bc6ccac87dd6608c9c2780
AaronXuHuang <aaron.huang@greendotcorp.com>
2022-08-18 22:58:40 +0800

    api get jira fix version
    RE-12345: AaronXuHuang <aaron.huang@greendotcorp.com>

--- ReleaseManagementProcess/app_jira/views.py
+++ ReleaseManagementProcess/app_jira/views.py
@@ -17,14 +17,23 @@ def test(request):
 
 
 def UpdateJiraProject(request):
+
+    projects = GetJiraProject()
+    PurgeTable(JiraProject)
+    SaveProjects(projects)
+
+    return JsonResponse(projects)
+
+
+def GetJiraProject():
     # https://pd.nextestate.com/rest/api/2/project
-    bulk_data = []
-    projects = {'projects': []}
+
+    projects = {
+        'projects': []
+        }
     url = jira_server + "/rest/api/2/project/"
 
-    #ids_in_db = list(JiraProject.objects.values_list('id', flat=True))
-    JiraProject.objects.all().delete()
-    items = requests.get( url=url, auth=(jira_user, jira_pwd), verify=False).json()
+    items = requests.get(url=url, auth=(jira_user, jira_pwd), verify=False).json()
     for item in items:
         project = {
             'id': item['id'],
@@ -33,12 +42,45 @@ def UpdateJiraProject(request):
         }
         projects['projects'].append(project)
 
+    return projects
+
+
+def PurgeTable(model):
+    model.objects.all().delete()
+
+
+def SaveProjects(projects):
+    bulk_data = []
+
     for project in projects['projects']:
         bulk_data.append(JiraProject(id=project['id'], name=project['name'], key=project['key']))
     JiraProject.objects.bulk_create(bulk_data)
 
-    return JsonResponse(projects)
 
+def GetJiraFixVersion(request):
+    # https://pd.nextestate.com/rest/api/2/project/12500/version?maxResults=1048576
+
+    fix_versions = {
+        'fixversions': []
+    }
+    project = request.GET.get('project')
+    projectid = JiraProject.objects.get(key=project).id
+    url = jira_server + "/rest/api/2/project/" + str(projectid) + '/version?maxResults=1048576'
+
+    items = requests.get(url=url, auth=(jira_user, jira_pwd), verify=False).json()
+    for item in items['values']:
+        if 'description' in item:
+            fix_versions['fixversions'].append({
+                'name': item['name'],
+                'description': item['description']
+                })
+        else:
+            fix_versions['fixversions'].append({
+                'name': item['name'],
+                'description': ""
+                })
+
+    return JsonResponse(fix_versions)
 
 def FetchIssue(request, orgunit, milestone):
     
