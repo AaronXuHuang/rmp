e98cbdeacfd9d478497b71d1f372b5c3238e9071
AaronXuHuang <aaron.huang@greendotcorp.com>
2022-08-20 11:08:38 +0800

    api update octo project
    RE-12345: AaronXuHuang <aaron.huang@greendotcorp.com>

--- ReleaseManagementProcess/app_octopus/models.py
+++ ReleaseManagementProcess/app_octopus/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class OctoSpace(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=50, null=True, blank=True)
+
+class OctoProject(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    projectgroupid = models.CharField(max_length=50, null=True, blank=True)
+    spaceid = models.CharField(max_length=50, null=True, blank=True)
--- ReleaseManagementProcess/app_octopus/views.py
+++ ReleaseManagementProcess/app_octopus/views.py
@@ -1,13 +1,12 @@
-from app_octopus.models import OctoSpace
+from app_octopus.models import OctoProject, OctoSpace
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import requests
 
 # Create your views here.
-OCTOPUS_USER='svc_mcp'
-OCTOPUS_PWD='Wa7rUxeD'
-OCTOPUS_SERVER='https://octopus.nextestate.com'
+# svc_mcp api key
 OCTOPUS_API_KEY='API-BDUFSI5UEGU3SOTLCH6IBDXFW'
+OCTOPUS_SERVER='https://octopus.nextestate.com'
 
 def UpdateSpace(request):
     
@@ -15,6 +14,19 @@ def UpdateSpace(request):
     OctoSpace.objects.all().delete()
     SaveSpace(spaces)
     return JsonResponse(spaces)
+
+
+def UpdateProject(request):
+    # Spaces-2: TPG, Spaces-22: RushCard, Spaces-22: RE
+    exclude_items = ['Spaces-2', 'Spaces-22', 'Spaces-62']
+    space_ids = list(OctoSpace.objects.values_list('id', flat=True))
+    for item in exclude_items:
+        space_ids.remove(item)
+    
+    projects = FetchProject(space_ids)
+    OctoProject.objects.all().delete()
+    SaveProject(projects)
+    return JsonResponse(projects)
 
 
 def FetchSpace():
@@ -36,12 +48,47 @@ def FetchSpace():
     return spaces
 
 
+def FetchProject(space_ids):
+    projects = {
+        'projects': [],
+    }
+
+    for space_id in space_ids:
+        url = OCTOPUS_SERVER + "/api/" + space_id + "/projects?skip=0&take=2147483647"
+        headers = {'X-Octopus-ApiKey': OCTOPUS_API_KEY}
+        items = requests.get(url=url, headers=headers, verify=False, allow_redirects=True).json()
+
+        for item in items['Items']:
+            projects['projects'].append({
+                'id': item['Id'],
+                'name': item['Name'],
+                'projectgroupid': item['ProjectGroupId'],
+                'spaceid': item['SpaceId']
+            })
+
+    return projects
+
+
 def SaveSpace(spaces):
     bulk_data = []
 
     for space in spaces['spaces']:
-        bulk_data.append(OctoSpace(id=space['id'], name=space['name']))
+        bulk_data.append(OctoSpace(
+            id=space['id'], 
+            name=space['name']))
     OctoSpace.objects.bulk_create(bulk_data)
+
+
+def SaveProject(projects):
+    bulk_data = []
+
+    for project in projects['projects']:
+        bulk_data.append(OctoProject(
+            id=project['id'], 
+            name=project['name'], 
+            projectgroupid=project['projectgroupid'],
+            spaceid=project['spaceid']))
+    OctoProject.objects.bulk_create(bulk_data)
 
 
 def FetchEnvironment(request, orgunit):
