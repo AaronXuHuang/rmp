9199a07a2d42289c4a5c2eff27980ff0396da6fc
AaronXuHuang <aaron.huang@greendotcorp.com>
2022-08-25 22:01:57 +0800

    refactor create ro
    RE-12345: AaronXuHuang <aaron.huang@greendotcorp.com>

--- ReleaseManagementProcess/app_releaseobject/views.py
+++ ReleaseManagementProcess/app_releaseobject/views.py
@@ -15,56 +15,15 @@ def CreateRO(request):
     # fetch jira issues according to the fix version
     release_object = {fix_version: {}}
     issues = Jiraviews.FetchJiraIssues(orgunit, fix_version)
-    
     release_object = ConstructROComponent(fix_version, issues, release_object)
     release_object = ConstructROIssue(fix_version, issues, release_object)
-
     # get octopus space id
     octo_space_id = Octoviews.GetOrgunitSpaceId(orgunit)
-
     # get octopus project id
-    octo_project_id_map = {}
-    project_names = []
-    for component in release_object[fix_version]:
-        project_names.append(component)
-    octo_projects= Octoviews.GetProjectId(project_names)
-    for octo_project in octo_projects:
-        octo_project_id_map[octo_project['name']] = octo_project['id']
-
+    octo_project_id_map = ConstructROProjectMap(release_object, fix_version)
     # fetch octopus project environments (run for the first component)
-    project_name = list(octo_project_id_map)[0]
-    channel_environments = Octoviews.FetchProjectChannelEnvironments(
-        octo_space_id,
-        octo_project_id_map[project_name])
-    default_channel = channel_environments['default']
-
-    environments = {}
-    for environment in channel_environments['channels'][default_channel]:
-        environments[environment['Name']] = environment['Id']
-
-    for project_name in release_object[fix_version]:
-        # fetch octopus projcet release
-        project_id = octo_project_id_map[project_name]
-        releases = Octoviews.FetchProjectReleases(octo_space_id, project_id)
-
-        releases_filtered = {'releases': {}}
-        for release in releases['releases']:
-            for jira_issue in release_object[fix_version][project_name]:
-                if releases['releases'][release]['jiraissue'] == jira_issue:
-                    releases_filtered['releases'][release] = releases['releases'][release]
-
-        # fetch octopus projcet release deployment
-        releases_filtered, tasks = Octoviews.FetchProjectReleaseDeployments(octo_space_id, releases_filtered)
-
-        # not necessary to check deployments state
-        # because if the deployment failed, the jira issue can not be finished
-        # fetch octopus projcet release deployment state
-        releases_filtered = Octoviews.FetchProjectReleaseDeploymentStates(releases_filtered, tasks)
-
-        for release in releases_filtered['releases']:
-            for jira_issue in release_object[fix_version][project_name]:
-                if releases_filtered['releases'][release]['jiraissue'] == jira_issue:
-                    release_object[fix_version][project_name][jira_issue]['releases'][release] = releases_filtered['releases'][release]
+    environments = ConstructROEnvironmentMap(octo_space_id, octo_project_id_map)
+    release_object = ConstructRO(release_object, fix_version, octo_space_id, octo_project_id_map)
  
     return JsonResponse(release_object)
 
@@ -106,3 +65,70 @@ def ConstructROIssue(fix_version, issues, release_object):
     return release_object
 
 
+def ConstructROProjectMap(release_object, fix_version):
+    octo_project_id_map = {}
+    project_names = []
+
+    for component in release_object[fix_version]:
+        project_names.append(component)
+    octo_projects= Octoviews.GetProjectId(project_names)
+    for octo_project in octo_projects:
+        octo_project_id_map[octo_project['name']] = octo_project['id']
+
+    return octo_project_id_map
+
+
+def ConstructROEnvironmentMap(octo_space_id, octo_project_id_map):
+    environments = {}
+    project_name = list(octo_project_id_map)[0]
+
+    channel_environments = Octoviews.FetchProjectChannelEnvironments(
+        octo_space_id,
+        octo_project_id_map[project_name])
+    default_channel = channel_environments['default']
+
+    for environment in channel_environments['channels'][default_channel]:
+        environments[environment['Id']] = environment['Name']
+    
+    return environments
+
+
+def FilterRelease(octo_project_id_map, project_name, octo_space_id, release_object, fix_version):
+    # fetch octopus projcet release
+    project_id = octo_project_id_map[project_name]
+    releases_filtered = {'releases': {}}
+
+    releases = Octoviews.FetchProjectReleases(octo_space_id, project_id)
+    
+    for release in releases['releases']:
+        for jira_issue in release_object[fix_version][project_name]:
+            if releases['releases'][release]['jiraissue'] == jira_issue:
+                releases_filtered['releases'][release] = releases['releases'][release]
+    
+    return releases_filtered
+    
+
+def ConstrucRelease(releases_filtered, release_object, fix_version, project_name):
+    for release in releases_filtered['releases']:
+        for jira_issue in release_object[fix_version][project_name]:
+            if releases_filtered['releases'][release]['jiraissue'] == jira_issue:
+                release_object[fix_version][project_name][jira_issue]['releases'][release] = releases_filtered['releases'][release]
+
+    return release_object
+
+
+def ConstructRO(release_object, fix_version, octo_space_id, octo_project_id_map):
+    for project_name in release_object[fix_version]:
+        releases_filtered = FilterRelease(octo_project_id_map, project_name, octo_space_id, release_object, fix_version)
+
+        # fetch octopus projcet release deployment
+        releases_filtered, tasks = Octoviews.FetchProjectReleaseDeployments(octo_space_id, releases_filtered)
+
+        # not necessary to check deployments state
+        # because if the deployment failed, the jira issue can not be finished
+        # fetch octopus projcet release deployment state
+        releases_filtered = Octoviews.FetchProjectReleaseDeploymentStates(releases_filtered, tasks)
+
+        release_object = ConstrucRelease(releases_filtered, release_object, fix_version, project_name)
+
+    return release_object
